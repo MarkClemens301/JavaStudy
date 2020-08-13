e3476cb47e4ce4ec2e14be7eab60883770f07f10
MarkClemens301 <liusheng302@163.com>
2020-08-13 23:59:16 +0800

    2020-8-13 列表转换为基本类型数组（不是包装类型数组）

--- BasicGrammar/工具类使用/List2Array.java
+++ BasicGrammar/工具类使用/List2Array.java
@@ -14,13 +14,24 @@ public class List2Array {
         for (int i = 0; i < 10; i++) {
             list.add((double) i);
         }
-        Double[] arr = null;
 
+        System.out.println("Convert List to Array (Double[] double[])");
+
+        //方式一
+        Double[] arr = null;
         arr = list.toArray(new Double[0]);//todo list转为array
         System.out.println(Arrays.toString(arr));
+        //方式二 to
+        double[] arr2 = null;
+        arr2 = list.stream().mapToDouble(Double::valueOf).toArray(); //todo list2array
+        System.out.println(Arrays.toString(arr2));
+
+        System.out.println("Sort List");
 
         Arrays.sort(arr, (x, y) -> y.compareTo(x)); //todo 递减排序
         System.out.println(Arrays.toString(arr));
+
+        System.out.println("Find max or min value of a List");
 
         System.out.println(Collections.max(list));
         System.out.println(Collections.min(list));
