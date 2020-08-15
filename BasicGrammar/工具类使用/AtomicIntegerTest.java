dbfca5adb1e0caf3438dc2c0312418a59f3ac8a3
MarkClemens301 <liusheng302@163.com>
2020-08-15 12:42:53 +0800

    2020-8-15 AtomicInteger 线程安全容器 前加加后加加...

--- BasicGrammar/工具类使用/AtomicIntegerTest.java
+++ BasicGrammar/工具类使用/AtomicIntegerTest.java
@@ -8,13 +8,22 @@ public class AtomicIntegerTest {
     比synchronized更加高效
     获取、比较、设置
      */
-    private static AtomicInteger serialNum = new AtomicInteger(10);
+    private static AtomicInteger serialNum = new AtomicInteger(5);
 
     static {
-        System.out.println("原数字：" + serialNum);
-        serialNum.getAndIncrement(); //相当于a++
-        System.out.println("自增后：" + serialNum);
+        System.out.println("原数字：" + serialNum + "\n");
 
+        serialNum.set(5);
+        System.out.println("自增后（后加加）：" + serialNum.getAndIncrement()); //相当于a++
+        serialNum.set(5);
+        System.out.println("自增后（前加加）：" + serialNum.incrementAndGet()); //相当于++a
+
+        System.out.println();
+
+        serialNum.set(5);
+        System.out.println("自减后（后减减）：" + serialNum.getAndDecrement()); //相当于a--
+        serialNum.set(5);
+        System.out.println("自减后（前减减）：" + serialNum.decrementAndGet()); //相当于--a
     }
 
     public static void main(String[] args) {
