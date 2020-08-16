1f8e9f529519313d300eb0e34f21f0a627a38bcb
MarkClemens301 <liusheng302@163.com>
2020-08-16 14:23:58 +0800

    2020-8-16 设计模式：单例模式

--- 设计模式/DM.java
+++ 设计模式/DM.java
@@ -1,5 +1,5 @@
 public class DM {
     public static void main(String[] args) {
-        System.out.println("Design Mode");
+        System.out.println("Design Mode/Pattern");
     }
 }
---
+++ 设计模式/DM01_单例模式/单例模式.java
@@ -0,0 +1,24 @@
+package DM01_单例模式;
+
+public class 单例模式 {
+// TODO: 2020/8/16 Singleton Pattern
+
+    private static final 单例模式 singleton = new 单例模式();
+
+    private 单例模式() {
+    }
+
+    public static 单例模式 getSingleton() {
+        System.out.println("返回单例");
+        return singleton;
+    }
+
+    public static void doSth() {
+    }
+}
+
+class testSingleton {
+    public static void main(String[] args) {
+        单例模式 s = 单例模式.getSingleton();
+    }
+}
