a5c86129c1f1cb5036df78da148052f04488e17a
MarkClemens301 <liusheng302@163.com>
2020-08-20 09:41:06 +0800

    2020-8-20 stream().filter(..).count() test

--- 基础语法/工具类/StreamTool.java
+++ 基础语法/工具类/StreamTool.java
@@ -3,7 +3,10 @@ package 工具类;/* 2020/8/20 8:59 */
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StreamTool {
     @Test
@@ -15,9 +18,25 @@ public class StreamTool {
         list.add(4);
         list.add(null);
         list.add(6);
-        //stream创建 filter转换 count聚合
+        //Stream: stream创建 filter转换 count聚合
 
-        int countNull = (int) list.stream().filter(num -> num == null).count();
+        int countNull;
+        countNull = (int) list.stream()
+                .filter(num -> num == null).count();
         System.out.println("countNull = " + countNull);
+
+
+        //静态创建Stream  of
+        Stream iS = Stream.of(1, 2, 3, 5, 3);
+        Stream sS = Stream.of("taobao");
+
+        //转换Stream  distinct filter
+        String dis = (String) iS.distinct()
+                .map(x -> x.toString())
+                .collect(Collectors.joining(","));
+        //汇聚 count reduce collect
+        System.out.println(dis);
+
+        sS.forEach(System.out::println);
     }
 }
