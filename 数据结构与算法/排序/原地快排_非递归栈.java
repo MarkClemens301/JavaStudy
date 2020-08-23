fd55fbf29d2134994d628f139d1bea749464bae3
MarkClemens301 <liusheng302@163.com>
2020-08-23 10:58:57 +0800

    2020-8-23 非递归原地快排，手写

--- 数据结构与算法/排序/原地快排_非递归栈.java
+++ 数据结构与算法/排序/原地快排_非递归栈.java
@@ -3,6 +3,7 @@ package 排序;/* 2020/8/21 9:22 */
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 public class 原地快排_非递归栈 {
     @Test
@@ -14,6 +15,42 @@ public class 原地快排_非递归栈 {
 
     public void quickSort(int[] nums) {
         //quickSort(nums, 0, nums.length - 1);
+        int left = 0, right = nums.length - 1;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(left);
+        stack.push(right);
+        while (!stack.isEmpty()) {
+            int high = stack.pop();
+            int low = stack.pop();
+            int pivot = partition(nums, low, high);
+            if (low < pivot - 1) { //务必判断这个条件，否则溢出
+                stack.push(low);
+                stack.push(pivot - 1);
+            }
+            if (pivot + 1 < high) {
+                stack.push(pivot + 1);
+                stack.push(high);
+            }
+        }
     }
     // TODO: 2020/8/21 用栈实现的非递归快排，下回再写
+
+    private int partition(int[] nums, int left, int right) {
+        int pivot = left, index = pivot + 1;
+        for (int i = index; i <= right; i++) { //注意：一定要理解
+            //遇到小值，小值和大值位置互换
+            if (nums[i] < nums[pivot]) {
+                if (i != index) swap(nums, i, index);
+                index++; //下一个大值
+            }
+        }
+        swap(nums, pivot, index - 1);
+        return index - 1;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int val = nums[i];
+        nums[i] = nums[j];
+        nums[j] = val;
+    }
 }
