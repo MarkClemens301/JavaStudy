924b9f66f69d619de63cfdb47e489a74ff67aaf7
MarkClemens301 <liusheng302@163.com>
2020-08-23 10:28:52 +0800

    2020-8-23 Java正则表达式（特殊），如匹配"\"(\\)要用"\\\\"

--- 基础语法/工具类/StringTool.java
+++ 基础语法/工具类/StringTool.java
@@ -13,7 +13,11 @@ public class StringTool {
         }
         String s = Arrays.toString(arr);
         System.out.println("the old: " + s);
-        s.replaceAll("[],]", "");
+        // TODO: 2020/8/23 regex in java:  特殊，尽量避免使用，免得出错
+        //  匹配"\"用\\\\; 匹配"\\"用\\\\\\\\;
+        //  匹配数字 用\\d; 匹配"("用\\(
+        //s.replaceAll("[\\[,]]", "");
+        s = s.replaceAll("[\\[,\\]]", "");
         System.out.println("the new: " + s);
     }
 }
