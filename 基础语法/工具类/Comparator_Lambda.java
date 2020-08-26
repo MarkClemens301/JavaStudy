6047225eddd905f973f2d6c8e6787720fb120035
MarkClemens301 <liusheng302@163.com>
2020-08-26 12:54:25 +0800

    2020-8-26 lambda表达式 排序时，很慢

--- 基础语法/工具类/Comparator_Lambda.java
+++ 基础语法/工具类/Comparator_Lambda.java
@@ -6,10 +6,16 @@ public class Comparator_Lambda {
     public static void main(String[] args) {
         Integer[] nums;
 
+        long t1 = System.currentTimeMillis();
+
         //lambda表达式创建Comparator性能会低一些
         nums = new Integer[]{1, 5, 9, 8, 5, 3, 5, 5, 6, 9};
         Arrays.sort(nums, (x, y) -> (y - x)); //升序排序
         System.out.println(Arrays.toString(nums));
+
+        long t2 = System.currentTimeMillis();
+        System.out.println("(t2 - t1) = " + (t2 - t1));
+
 
         //普通创建Comparator语句，建议
         nums = new Integer[]{1, 5, 9, 8, 5, 3, 5, 5, 6, 9};
@@ -20,5 +26,10 @@ public class Comparator_Lambda {
             }
         });
         System.out.println(Arrays.toString(nums));
+
+        long t3 = System.currentTimeMillis();
+        System.out.println("(t3 - t2) = " + (t3 - t2));
+
+        System.out.println("lambda表达式 比普通创建Comparator慢很多！");
     }
 }
