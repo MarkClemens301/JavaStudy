fc507cbc28152b185a5fa558bd3f98529306599e
MarkClemens301 <liusheng302@163.com>
2020-08-28 01:03:29 +0800

    2020-8-28 java.time.LocalDateTime;

--- 核心基础/_9常用类/时间API.java
+++ 核心基础/_9常用类/时间API.java
@@ -3,8 +3,29 @@ package _9常用类;/* 2020/8/28 0:23 */
 import org.junit.Test;
 
 import java.util.*; //import java.io.*;
-
+import java.time.*;
+/*
+第一代 jdk1.0 java.util.Date
+第二代 jdk1.1 java.util.Calendar
+第三代 jdk1.8 java.time
+ */
 public class 时间API {
+    @Test
+    public void new_API_time() {
+        //import java.time.*;
+        LocalDate ld = LocalDate.now();
+        System.out.println("ld = " + ld);
+        LocalDate of = LocalDate.of(2017, 8, 13);
+        System.out.println(of);
+
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println("now = " + now);
+
+        System.out.println("now.getDayOfWeek() = " + now.getDayOfWeek());
+        System.out.println("now.getMonth() = " + now.getMonth());
+        System.out.println("now.getDayOfYear() = " + now.getDayOfYear());
+    }
+
     @Test
     public void canlendarT() {
         Calendar cl = Calendar.getInstance();
@@ -16,5 +37,9 @@ public class 时间API {
 
         double timePercent = 1.0 / 365 * Calendar.getInstance().get(Calendar.DAY_OF_YEAR);
         System.out.println("timePercent = " + timePercent);
+
+        int dayofweek = Calendar.getInstance().get(Calendar.DAY_OF_WEEK);
+        System.out.println("dayofweek = " + dayofweek);
     }
+
 }
