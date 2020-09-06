59e53b2d16b7a4078a0b6b3e25ef3d6e84a3227f
MarkClemens301 <liusheng302@163.com>
2020-09-07 03:46:42 +0800

    集合求交集，测试

--- 数据结构与算法/数据结构/集合Set/集合_交并补.java
+++ 数据结构与算法/数据结构/集合Set/集合_交并补.java
@@ -8,18 +8,22 @@ public class 集合_交并补 {
     private Set<Integer> set1, set2, set;
 
     public 集合_交并补() {
+        System.out.println("Constructor");
         set1 = new HashSet<>();
         set2 = new HashSet<>();
         set = new HashSet<>();
 
         set1.addAll(Arrays.asList(1, 2, 3, 5));
-        set1.addAll(Arrays.asList(2, 3, 4));
+        set2.addAll(Arrays.asList(2, 3, 4));
+        System.out.println(set1);
+        System.out.println(set2);
     }
 
     @Test
     public void 交集() {
+        //new 集合_交并补();
         set.addAll(set1);
         set.retainAll(set2);
-        System.out.println(set);
+        System.out.println("InterSection: " + set);
     }
 }
