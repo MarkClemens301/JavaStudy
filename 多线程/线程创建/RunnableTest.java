7a127aa6cb3f150b3e7ba0125a9994b087b8a27c
MarkClemens301 <liusheng302@163.com>
2020-09-06 10:57:44 +0800

    Thread extends test

---
+++ 多线程/线程创建/RunnableTest.java
@@ -0,0 +1,7 @@
+/* 2020/9/6 10:45 */
+package 线程创建;/* 2020/9/6 10:45 */
+
+import java.util.*; //import java.io.*;
+
+public class RunnableTest {
+}
---
+++ 多线程/线程创建/ThreadTest.java
@@ -0,0 +1,40 @@
+/* 2020/9/6 10:45 */
+package 线程创建;/* 2020/9/6 10:45 */
+
+import java.util.*; //import java.io.*;
+
+public class ThreadTest {
+    public static void main(String[] args) {
+        //my
+        MyThread mt = new MyThread();
+        System.out.println(mt.getName());
+        mt.start();
+        //main
+        Thread thread = Thread.currentThread();
+        System.out.println(thread.getName());
+        thread.setPriority(1);//优先级更高，概率意义上的。 1~10
+        for (int i = 0; i < 100; i++) {
+            if ((i & 1) == 0) {
+                System.out.println(thread.getName()
+                        + ":" + thread.getPriority()
+                        + ":" + i);
+            }
+        }
+    }
+}
+
+class MyThread extends Thread {
+    @Override
+    public void run() {
+        System.out.println("super.run()");
+        super.run();
+        System.out.println("my run()");
+        for (int i = 0; i < 100; i++) {
+            if ((i & 1) == 0) {
+                System.out.println(getName() +
+                        ":" + getPriority() +
+                        ":" + i);
+            }
+        }
+    }
+}
