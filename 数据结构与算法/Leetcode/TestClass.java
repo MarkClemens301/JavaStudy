5dd5bde353542adcd118995f811a529357f5d88a
MarkClemens301 <liusheng302@163.com>
2020-09-09 01:33:35 +0800

    创建常用数据结构 ListNode TreeNode

---
+++ 数据结构与算法/Leetcode/TestClass.java
@@ -0,0 +1,11 @@
+package Leetcode;/* 2020/9/9 1:31 */
+
+import org.junit.Test;
+
+public class TestClass {
+    @Test
+    public void test() {
+        ListNode listNode = new ListNode();
+        System.out.println(listNode.val);
+    }
+}
--- 数据结构与算法/剑指Offer复习/Solution20.java
+++ 数据结构与算法/剑指Offer复习/Solution20.java
@@ -2,7 +2,7 @@ package 剑指Offer复习;/* 2020/9/8 14:49 */
 
 import org.junit.Test;
 
-//import org.junit.Test;
+//import org.junit.TestClass;
 //import java.util.*;
 public class Solution20 {
     private int n;
