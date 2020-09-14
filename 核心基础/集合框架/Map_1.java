74e4811da27ec2401e33753e807607c5ec6c7ce0
MarkClemens301 <liusheng302@163.com>
2020-09-15 01:26:53 +0800

    集合框架 Hashtable与HashMap null值区别、fail-safe

--- 核心基础/集合框架/Map_1.java
+++ 核心基础/集合框架/Map_1.java
@@ -11,6 +11,9 @@ public class Map_1 {//
      * 源码
      * HashMap 的null 处理
      * return (key == null) ? 0 : (h = key.hashCode()) ^ (h >>> 16);
+     * 区别：
+     * 对null 值的容忍度不一样，HashMap 允许键值均为null, 因为hash 时会进行处理
+     * Hashtable 使用fail-safe 机制，读到的数据不一定是最新的数据。
      */
     @Test
     public void t() {
