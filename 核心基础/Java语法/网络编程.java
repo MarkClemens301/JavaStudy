f32a558df5ae5ce496a05b05b71cc03431a6d3a4
MarkClemens301 <liusheng302@163.com>
2020-09-16 01:07:26 +0800

    TCP网络编程，服务端

--- 核心基础/Java语法/网络编程.java
+++ 核心基础/Java语法/网络编程.java
@@ -2,9 +2,12 @@ package Java语法;/* 2020/9/15 23:55 */
 
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetAddress;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 public class 网络编程 {//
@@ -26,10 +29,10 @@ class Client {
             //1. 创建Socket 对象，指明服务器的IP 和端口号
             inet = InetAddress.getByName("192.168.14.100");
             socket = new Socket(inet, 8899);
-            //2. 获取输出流，用于输出数据
-            os = socket.getOutputStream();
+            //2. 获取输出流
+            os = socket.getOutputStream();//
             //3. 数据的写操作
-            os.write("你好，我是客户mk".getBytes());//
+            os.write("你好，我是客户mk".getBytes());
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
@@ -44,6 +47,50 @@ class Client {
             if (socket != null) {
                 try {
                     socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
+
+class Server {
+    //服务端从客户端接收数据
+    public void server() {
+        ServerSocket ss = null;
+        Socket socket = null;
+        InputStream is = null;
+        ByteArrayOutputStream baos = null;
+        try {
+            //1. 创建服务端的ServerSocket 指明自己的端口号
+            ss = new ServerSocket(8899);
+            //2. 接收来自客户端的Socket
+            socket = ss.accept();
+            //3. 获取输入流
+            is = socket.getInputStream();//
+            //4. 读取输入流中的数据
+            baos = new ByteArrayOutputStream();//
+            byte[] buffer = new byte[5];
+            int len;
+            while ((len = is.read(buffer)) != -1) {
+                baos.write(buffer, 0, len);//
+            }
+            System.out.println(baos.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            //5. 关闭资源
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (ss != null) {
+                try {
+                    ss.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
