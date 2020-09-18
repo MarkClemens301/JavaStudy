14520a8c028cefd7e2d89781bfe9bdb2c77d9cb9
MarkClemens301 <liusheng302@163.com>
2020-09-18 13:10:34 +0800

    数据结构 源码

--- 核心基础/集合框架/数据结构.java
+++ 核心基础/集合框架/数据结构.java
@@ -3,13 +3,16 @@ package 集合框架;/* 2020/9/16 10:09 */
 import org.junit.Test;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class 数据结构 {//
 
     @Test
     public void 数据结构() {
-        HashMap<Object, Object> map = new HashMap<>();
+        HashMap<Object, Object> map = new HashMap<>();//Node
+        new ConcurrentHashMap<>();/*putVal*/
+        new LinkedHashMap<>();//Entry
         new Object();
         new ConcurrentHashMap<>();
     }
