8f099dc8b2be6bb71016ee5970d5a1ab9c6ae9d0
MarkClemens301 <liusheng302@163.com>
2020-09-19 01:15:37 +0800

    回溯搜索路径

---
+++ 数据结构与算法/数据结构/前缀树/Tire.java
@@ -0,0 +1,4 @@
+package 数据结构.前缀树;/* 2020/9/19 0:51 */
+
+public class Tire {//
+}
--- 数据结构与算法/算法/搜索/深度优先搜索.java
+++ 数据结构与算法/算法/搜索/深度优先搜索.java
@@ -1,9 +1,38 @@
 package 算法.搜索;/* 2020/8/31 1:11 */
 
-import java.util.*; //import java.io.*;
-
 public class 深度优先搜索 {
     public static void main(String[] args) {
-        System.out.println("TODO");
+        深度优先搜索 m = new 深度优先搜索();
+        m.tarx = 1;
+        m.tary = 2;
+        m.step = Integer.MAX_VALUE;//
+        boolean[][] map = {
+                {true, false, true},
+                {true, true, true},
+                {false, true, true},
+        };
+        m.tracking(map, 0, 0, 0);
+        System.out.println(m.step);
+    }
+
+    private int tarx, tary, step;
+    int[][] dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+    boolean tracking(boolean[][] map, int x, int y, int step) {//用深搜(回溯)来找最短距离试试
+        if (!map[x][y]) return false;
+        if (x == tarx && y == tary) {
+            System.out.println(step);//
+            this.step = Math.min(this.step, step);
+            return true;
+        }
+        map[x][y] = false;
+        for (int[] d : dirs) {
+            int nx = x + d[0], ny = y + d[1];
+            if (nx >= 0 && nx < map.length && ny >= 0 && ny < map[0].length) {
+                if (tracking(map, nx, ny, step + 1)) ;//return true;
+            }
+        }
+        map[x][y] = true;
+        return false;
     }
 }
