5431c486071ba605cd1ffcbb253c123484535953
MarkClemens301 <liusheng302@163.com>
2020-10-05 13:29:42 +0800

    JVM 查看java进程 "jps -lvm" 搜索结果中的project.path即可知道code所在的文件夹

--- 核心基础/JVM/调优/JPS.java
+++ 核心基础/JVM/调优/JPS.java
@@ -2,7 +2,8 @@ package JVM.调优;/* 2020/10/5 12:52 */
 
 /**
  * 查看Java进程：jps -lvm //jvm process status tool //类似于unix中的ps命令
- * 查看某个进程下的堆栈情况： jstack -l pid(如 12140)
+ * 结果中找到 12012 ... -Dpreload.project.path=D:/Public/Documents/GitHub/JavaStudy ...
+ * 查看某个进程下的堆栈情况： jstack -l pid(线程id, 如12012)
  */
 public class JPS {//
     public static void main(String[] args) throws InterruptedException {
