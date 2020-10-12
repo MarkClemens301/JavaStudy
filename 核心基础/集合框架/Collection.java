76a976e8e25783418a9cf2f82303674a5426f805
MarkClemens301 <liusheng302@163.com>
2020-10-13 00:39:15 +0800

    集合框架 Collection(接口) 与 Collections(静态工具类) 区别

---
+++ 核心基础/集合框架/Collection.java
@@ -0,0 +1,14 @@
+package 集合框架;/* 2020/10/13 0:37 */
+
+import java.util.ArrayList;
+
+public class Collection {//
+
+    public static void main(String[] args) {
+        ArrayList<Float> list = new ArrayList<>();
+        list.add(12F);
+        list.add(2.6F);
+        list.add(2.566666F);
+        System.out.println(list);
+    }
+}
