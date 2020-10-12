dc66d6118dd88cf8d80e3b5606a6524114f6172a
MarkClemens301 <liusheng302@163.com>
2020-10-13 00:45:51 +0800

    集合框架 Collection(接口) 与 Collections(工具类\普通类) 区别 纠正：Collections不是静态类。静态（内部）类可看做外部类的静态变量，不需要外部类实例。

--- 核心基础/集合框架/Collection.java
+++ 核心基础/集合框架/Collection.java
@@ -6,14 +6,15 @@ import java.util.Collections;
 public class Collection {//
 
     public static void main(String[] args) {
-        ArrayList<Float> list = new ArrayList<>();
+        ArrayList<Float> list = new ArrayList<>();//
         list.add(12F);
         list.add(2.6F);
         list.add(2.566666F);
         list.add(199.5f);
         System.out.println(list);
         //Sort
-        Collections.sort(list);
+        Collections.sort(list);//纠正 Collections是普通工具类，不是静态类。
         System.out.println(list);
+        //静态类不用先创建外部类，可看做外部类的静态变量。非静态就必须先实例化。
     }
 }
