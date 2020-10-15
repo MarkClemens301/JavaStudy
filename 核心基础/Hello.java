8cffcaa1d47b464af53ef732d348d975acea68e5
MarkClemens301 <liusheng302@163.com>
2020-10-16 00:16:05 +0800

    多线程 ReentrantLock(); 排他、可重入 ReentrantReadWriteLock(); 非独占式 CyclicBarrier CountDownLatch

--- 核心基础/Hello.java
+++ 核心基础/Hello.java
@@ -1,5 +1,7 @@
 import org.junit.Test;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
@@ -17,5 +19,7 @@ public class Hello {//
         }
         new ReentrantReadWriteLock();//读写锁、非独占式
         new ReentrantLock();//可重入锁、独占式
+        new CyclicBarrier(2);//内存屏障  +1直到指定值 可循环利用
+        new CountDownLatch(2);//闭锁  -1直到0
     }
 }
