049ffc78cbef68c59e1fe1758fbb2aa8cdbb65aa
MarkClemens301 <liusheng302@163.com>
2020-10-16 17:32:03 +0800

    守护线程 Daemon thread.setDaemon(true);

--- 核心基础/多线程/Hello.java
+++ 核心基础/多线程/Hello.java
@@ -5,12 +5,14 @@ import org.junit.Test;
 import java.util.concurrent.Callable;
 import java.util.concurrent.FutureTask;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class Hello {//
 
     @Test
-    public void t1() throws Exception {
+    public void t1() throws Exception {//线程的创建、使用
         AtomicInteger aInt = new AtomicInteger();//原子类
+        System.out.println(aInt);
         //1. Thread
         class MyThread extends Thread {
             public void run() {
@@ -37,5 +39,23 @@ public class Hello {//
         });
         new Thread(ft).start();//
         System.out.println(ft.get());
+        aInt.decrementAndGet();
+    }
+
+    @Test
+    public void t2() throws InterruptedException {//Daemon 线程
+        AtomicLong aLong = new AtomicLong();
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    aLong.addAndGet(i);
+                }
+            }
+        });
+        thread.setDaemon(true);
+        thread.start();
+        Thread.sleep(1);//1ms
+        System.out.println(aLong.get());
     }
 }
