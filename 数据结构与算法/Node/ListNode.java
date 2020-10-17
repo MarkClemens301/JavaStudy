6c7f7bc811c8bbd42407ea5174d12a823ceade16
MarkClemens301 <liusheng302@163.com>
2020-10-18 00:34:32 +0800

    public ListNode(int[] arr){} 构造函数

--- 数据结构与算法/Node/ListNode.java
+++ 数据结构与算法/Node/ListNode.java
@@ -11,17 +11,29 @@ public class ListNode {
         this.val = val;
     }
 
+    public ListNode(int[] ints) {//constructor
+        if(ints == null || ints.length<1) return;
+        ListNode cur = new ListNode(ints[0]);
+        ListNode head = cur;
+        for (int i = 1; i < ints.length; i++) {
+            cur.next = new ListNode(ints[i]);
+            cur = cur.next;
+        }
+        this.val = head.val;
+        this.next = head.next;
+    }
+
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        ListNode head = this;
-        while (head != null) {
-            sb.append(head.val).append(',');
-            head = head.next;
-        }
-        if (this != null) {
-            sb.deleteCharAt(sb.length() - 1);
-        }
+            ListNode head = this;
+            while (head != null) {
+                sb.append(head.val).append(',');
+                head = head.next;
+            }
+            if (this != null) {
+                sb.deleteCharAt(sb.length() - 1);
+            }
         return sb.toString();
     }
 }
--- 数据结构与算法/剑指Offer复习/_25_合并两个有序链表.java
+++ 数据结构与算法/剑指Offer复习/_25_合并两个有序链表.java
@@ -1,5 +1,40 @@
 package 剑指Offer复习;/* 2020/10/17 23:57 */
 
+import Node.ListNode;
+
 public class _25_合并两个有序链表 {//
 
+    public static void main(String[] args) {
+        _25_合并两个有序链表 m = new _25_合并两个有序链表();
+        ListNode n1 = new ListNode(new int[]{1, 2, 3});
+        ListNode n2 = new ListNode(new int[]{1, 3, 4});
+        ListNode mergeList = m.mergeTwoLists(n1, n2);
+        System.out.println(mergeList);
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(), cur = dummy;
+        while (l1 != null && l1 != null) {
+            if (l1.val <= l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        if (l1 != null) cur.next = l1;
+        if (l2 != null) cur.next = l2;
+        return dummy.next;
+    }
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode(int x) { val = x; }
+     * }
+     */
 }
