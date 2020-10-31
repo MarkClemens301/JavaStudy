d11d27ca3dcc85bdae233b50eabe05299007a082
MarkClemens301 <liusheng302@163.com>
2020-11-01 00:00:25 +0800

    leetcode 递归 _25_树的子结构 test false

--- 数据结构与算法/剑指Offer复习/_26_树的子结构.java
+++ 数据结构与算法/剑指Offer复习/_26_树的子结构.java
@@ -1,8 +1,19 @@
 package 剑指Offer复习;/* 2020/10/31 23:52 */
 
 import Node.TreeNode;
+import org.junit.Test;
 
 public class _26_树的子结构 {//
+
+    @Test
+    public void t() {
+        TreeNode A = new TreeNode(1);
+        A.left = new TreeNode(2);
+        A.right= new TreeNode(3);
+        TreeNode B = new TreeNode(3);
+        B.right = new TreeNode(1);
+        System.out.println(isSubStructure(A, B));
+    }
 
     public boolean isSubStructure(TreeNode A, TreeNode B) {
         //空树不是任意树的子树
