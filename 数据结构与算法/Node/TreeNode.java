2ad86bb74908ff3db358d4d0503f44406d459df5
MarkClemens301 <liusheng302@163.com>
2020-11-02 00:20:31 +0800

    leetcode 递归/迭代 26#二叉树的镜像

--- 数据结构与算法/Node/TreeNode.java
+++ 数据结构与算法/Node/TreeNode.java
@@ -1,5 +1,8 @@
 package Node;/* 2020/9/9 1:23 */
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class TreeNode {//全都公开，供其他包调用，省去getter、setter
     public int val = 0;
     public TreeNode left = null;
@@ -10,5 +13,22 @@ public class TreeNode {//全都公开，供其他包调用，省去getter、sett
 
     public TreeNode(int val) {
         this.val = val;
+    }
+
+    public String toString() {//2020-11-2
+        TreeNode root = this;
+        StringBuilder sb = new StringBuilder();
+        Queue q = new LinkedList();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            TreeNode node = (TreeNode) q.poll();
+            if (node != null) {
+                sb.append(',').append(node.val);
+                q.offer(node.left);
+                q.offer(node.right);
+            }
+        }
+        if (sb.length() != 0) sb.deleteCharAt(0);
+        return sb.toString();
     }
 }
--- 数据结构与算法/剑指Offer复习/_27_二叉树的镜像.java
+++ 数据结构与算法/剑指Offer复习/_27_二叉树的镜像.java
@@ -3,6 +3,8 @@ package 剑指Offer复习;/* 2020/11/1 23:58 */
 import Node.TreeNode;
 import org.junit.Test;
 
+import java.util.Stack;
+
 public class _27_二叉树的镜像 {//
 
     @Test
@@ -14,14 +16,32 @@ public class _27_二叉树的镜像 {//
         root.left.right = new TreeNode(3);
         root.right.left = new TreeNode(6);
         root.right.right = new TreeNode(9);
-        System.out.println(mirrorTree(root));
+        System.out.println(mirrorTree(root));//4-27-1369 //4-72-9631
+        System.out.println(mirrorTreeIter(root));//4-27-1369 //4-72-9631
     }
 
-    TreeNode mirrorTree(TreeNode root) {
+    TreeNode mirrorTree(TreeNode root) {//递归
         if (root == null) return root;
         TreeNode tmp = root.left;
         root.left = mirrorTree(root.right);
         root.right = mirrorTree(tmp);
+        return root;
+    }
+
+    TreeNode mirrorTreeIter(TreeNode root) {//辅助栈
+        if(root == null) return root;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            if (node != null) {
+                stack.push(node.left);//save
+                stack.push(node.right);
+                TreeNode tmp = node.left;//swap
+                node.left = node.right;
+                node.right = tmp;
+            }
+        }
         return root;
     }
 }
