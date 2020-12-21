89b2fcd85aeb093639f9aac316ca49c7894d7d6a
MarkClemens301 <liusheng302@163.com>
2020-12-21 23:57:27 +0800

    Create _32_1_从上到下打印二叉树.java

---
+++ 数据结构与算法/剑指Offer复习/_32_1_从上到下打印二叉树.java
@@ -0,0 +1,29 @@
+package 剑指Offer复习;/* 2020/12/21 23:53 */
+/*
+// 例如:
+//给定二叉树: [3,9,20,null,null,15,7],
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+//
+//
+// 返回：
+//
+// [3,9,20,15,7]
+ */
+import Node.TreeNode;
+
+import java.util.*;//
+
+public class _32_1_从上到下打印二叉树 {//
+
+    // TODO: 2020/12/21 广度优先搜索 BFS 借助队列
+    public int[] levelOrder(TreeNode root) {
+        if (root==null) return new int[0];
+        
+        return new int[0];
+    }
+}
