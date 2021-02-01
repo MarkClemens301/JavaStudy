d302d249f585d8d0077c00598a3bbeb5cc3bbe58
MarkClemens301 <liusheng302@163.com>
2021-02-02 00:01:04 +0800

    Create _38_字符串的排列.java

---
+++ 数据结构与算法/剑指Offer复习/_38_字符串的排列.java
@@ -0,0 +1,31 @@
+package 剑指Offer复习;/* 2021/2/1 23:54 */
+/*
+// 你可以以任意顺序返回这个字符串数组，但里面不能有重复元素。
+//
+//
+//
+// 示例:
+//
+// 输入：s = "abc"
+//输出：["abc","acb","bac","bca","cab","cba"]
+ */
+
+import org.junit.Test;
+
+import java.util.*;//
+
+public class _38_字符串的排列 {//
+
+    @Test
+    public void t() {
+    }
+
+    public void permutation(String s) {
+    }
+
+    public void dfs() {//回溯，
+    }
+
+    public void swap() {//字符位置交换
+    }
+}
