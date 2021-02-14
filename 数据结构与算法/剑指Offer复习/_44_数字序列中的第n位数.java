14b6b7e9236ea2dc2df06785b2fad1ae53ff583d
MarkClemens301 <liusheng302@163.com>
2021-02-14 22:09:10 +0800

    Update _44_数字序列中的第n位数.java

--- 数据结构与算法/剑指Offer复习/_44_数字序列中的第n位数.java
+++ 数据结构与算法/剑指Offer复习/_44_数字序列中的第n位数.java
@@ -5,6 +5,7 @@ package 剑指Offer复习;/* 2021/2/14 21:20 */
 //
 // 请写一个函数，求任意第n位对应的数字。
  */
+
 import org.junit.Test;
 
 import java.util.*;//
@@ -13,9 +14,12 @@ public class _44_数字序列中的第n位数 {//
 
     @Test
     public void ts() {
-        System.out.println(findNthDigit(9));
-        System.out.println(findNthDigit(11));
-        System.out.println(findNthDigit(13));
+        System.out.println(findNthDigit(9));//9
+        System.out.println(findNthDigit(11));//0
+        System.out.println(findNthDigit(19));//4
+        System.out.println(12);
+        System.out.println(findNthDigit(14));//1
+        System.out.println(findNthDigit(15));//2
     }
 
     //迭代，找规律，不断缩小n
@@ -23,6 +27,7 @@ public class _44_数字序列中的第n位数 {//
         //[1] 0 [9] 1--9 [90] 10-99 [900] 100-999 ...
         if (n <= 9) return n;
         long bit = 1, pow = 1, step = 9;
+        n -= 1;//TODO!! -[step=1]"0"
 
         while (n > step) {
             n -= step;//走完【step】后，还剩多少步
@@ -31,8 +36,8 @@ public class _44_数字序列中的第n位数 {//
             step = 9 * pow * bit;//eg. 9*10*2bit
         }
 
-        long num = pow + (n - 1) / bit;//2. find num
-        int place = (int) ((n - 1) % bit);
+        long num = pow + n / bit;//2. find num
+        int place = (int) (n % bit);
         return String.valueOf(num).charAt(place) - '0';
     }
 }
