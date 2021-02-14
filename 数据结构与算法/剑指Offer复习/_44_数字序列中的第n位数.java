bc4c97038da24c895ae69efb749c712df19a4453
MarkClemens301 <liusheng302@163.com>
2021-02-14 21:51:28 +0800

    Create _44_数字序列中的第n位数.java

---
+++ 数据结构与算法/剑指Offer复习/_44_数字序列中的第n位数.java
@@ -0,0 +1,38 @@
+package 剑指Offer复习;/* 2021/2/14 21:20 */
+/*
+//数字以0123456789101112131415…的格式序列化到一个字符序列中。在这个序列中，第5位（从下标0开始计数）是5，第13位是1，第19位是4，
+//等等。
+//
+// 请写一个函数，求任意第n位对应的数字。
+ */
+import org.junit.Test;
+
+import java.util.*;//
+
+public class _44_数字序列中的第n位数 {//
+
+    @Test
+    public void ts() {
+        System.out.println(findNthDigit(9));
+        System.out.println(findNthDigit(11));
+        System.out.println(findNthDigit(13));
+    }
+
+    //迭代，找规律，不断缩小n
+    public int findNthDigit(int n) {
+        //[1] 0 [9] 1--9 [90] 10-99 [900] 100-999 ...
+        if (n <= 9) return n;
+        long bit = 1, pow = 1, step = 9;
+
+        while (n > step) {
+            n -= step;//走完【step】后，还剩多少步
+            pow *= 10;//下一段的基数
+            bit += 1;//下一段每个数的位数长度
+            step = 9 * pow * bit;//eg. 9*10*2bit
+        }
+
+        long num = pow + (n - 1) / bit;//2. find num
+        int place = (int) ((n - 1) % bit);
+        return String.valueOf(num).charAt(place) - '0';
+    }
+}
